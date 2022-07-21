ff786e3ca298d1c0aac05699464995413c7af6f8
eistakovskii <104836394+eistakovskii@users.noreply.github.com>
2022-07-21 12:26:00 +0300

    Update useful_regex.py

--- useful_regex.py
+++ useful_regex.py
@@ -1,4 +1,4 @@
-# CODE FROM NLP SPECIALIZATION
+################################### CODE FROM NLP SPECIALIZATION #########################################
 
 import re
 import string
@@ -43,3 +43,42 @@ def process_tweet(tweet):
             tweets_clean.append(stem_word)
 
     return tweets_clean
+
+######################################## CODE FROM KAGGLE ###############################################
+
+# Source: https://www.kaggle.com/code/ludovicocuoghi/twitter-sentiment-analysis-with-bert-roberta/notebook
+
+##CUSTOM DEFINED FUNCTIONS TO CLEAN THE TWEETS
+
+#Clean emojis from text
+def strip_emoji(text):
+    return re.sub(emoji.get_emoji_regexp(), r"", text) #remove emoji
+
+#Remove punctuations, links, mentions and \r\n new line characters
+def strip_all_entities(text): 
+    text = text.replace('\r', '').replace('\n', ' ').replace('\n', ' ').lower() #remove \n and \r and lowercase
+    text = re.sub(r"(?:\@|https?\://)\S+", "", text) #remove links and mentions
+    text = re.sub(r'[^\x00-\x7f]',r'', text) #remove non utf8/ascii characters such as '\x9a\x91\x97\x9a\x97'
+    banned_list= string.punctuation + 'Ã'+'±'+'ã'+'¼'+'â'+'»'+'§'
+    table = str.maketrans('', '', banned_list)
+    text = text.translate(table)
+    return text
+
+#clean hashtags at the end of the sentence, and keep those in the middle of the sentence by removing just the # symbol
+def clean_hashtags(tweet):
+    new_tweet = " ".join(word.strip() for word in re.split('#(?!(?:hashtag)\b)[\w-]+(?=(?:\s+#[\w-]+)*\s*$)', tweet)) #remove last hashtags
+    new_tweet2 = " ".join(word.strip() for word in re.split('#|_', new_tweet)) #remove hashtags symbol from words in the middle of the sentence
+    return new_tweet2
+
+#Filter special characters such as & and $ present in some words
+def filter_chars(a):
+    sent = []
+    for word in a.split(' '):
+        if ('$' in word) | ('&' in word):
+            sent.append('')
+        else:
+            sent.append(word)
+    return ' '.join(sent)
+
+def remove_mult_spaces(text): # remove multiple spaces
+    return re.sub("\s\s+" , " ", text)
