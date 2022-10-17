7f7ac1495487b825526c5b9298cd9b8b5fca61b1
eistakovskii <104836394+eistakovskii@users.noreply.github.com>
2022-10-17 23:09:53 +0300

    uploaded bleu score

--- metrics_NMT.py
+++ metrics_NMT.py
@@ -71,3 +71,71 @@ def rouge1_similarity(system, reference):
         rouge1_score = 0 
     
     return rouge1_score
+
+import numpy as np                  # import numpy to make numerical computations.
+import nltk                         # import NLTK to handle simple NL tasks like tokenization.
+nltk.download("punkt")
+from nltk.util import ngrams
+from collections import Counter     # import the Counter module.
+# !pip3 install 'sacrebleu'           # install the sacrebleu package.
+import sacrebleu                    # import sacrebleu in order compute the BLEU score.
+
+def brevity_penalty(reference, candidate):
+    ref_length = len(reference)
+    can_length = len(candidate)
+
+    # Brevity Penalty
+    if ref_length > can_length:
+        BP = 1
+    else:
+        penalty = 1 - (ref_length / can_length)
+        BP = np.exp(penalty)
+
+    return BP
+
+def clipped_precision(reference, candidate):
+    """
+    Bleu score function given a original and a machine translated sentences
+    """
+
+    clipped_precision_score = []
+
+    for i in range(1, 5):
+        candidate_n_gram = Counter(ngrams(candidate, i))  # counts of n-gram n=1...4 tokens for the candidate
+        reference_n_gram = Counter(ngrams(reference, i))  # counts of n-gram n=1...4 tokens for the reference
+
+        c = sum(reference_n_gram.values())  # sum of the values of the reference the denominator in the precision formula
+
+        for j in reference_n_gram:  # for every n_gram token in the reference
+            if j in candidate_n_gram:  # check if it is in the candidate n-gram
+                if (reference_n_gram[j] > candidate_n_gram[j]):  # if the count of the reference n-gram is bigger
+                    # than the corresponding count in the candidate n-gram
+                    reference_n_gram[j] = candidate_n_gram[j]  # then set the count of the reference n-gram to be equal
+                    # to the count of the candidate n-gram
+            else:
+
+                reference_n_gram[j] = 0  # else reference n-gram = 0
+
+        clipped_precision_score.append(sum(reference_n_gram.values()) / c)
+
+    weights = [0.25] * 4
+
+    s = (w_i * np.log(p_i) for w_i, p_i in zip(weights, clipped_precision_score))
+    s = np.exp(np.sum(s))
+    return s
+
+def bleu_score(reference, candidate):
+    BP = brevity_penalty(reference, candidate)
+    precision = clipped_precision(reference, candidate)
+    return BP * precision
+
+
+
+# BLEU Score	Interpretation
+# < 10	Almost useless
+# 10 - 19	Hard to get the gist
+# 20 - 29	The gist is clear, but has significant grammatical errors
+# 30 - 40	Understandable to good translations
+# 40 - 50	High quality translations
+# 50 - 60	Very high quality, adequate, and fluent translations
+# > 60	Quality often better than human
