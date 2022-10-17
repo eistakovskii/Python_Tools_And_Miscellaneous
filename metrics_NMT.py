2c948b73b7363ee8b30945281d1ac81ea75e9c3b
eistakovskii <104836394+eistakovskii@users.noreply.github.com>
2022-10-17 11:19:37 +0300

    Update metrics_NMT.py

--- metrics_NMT.py
+++ metrics_NMT.py
@@ -26,7 +26,7 @@ def jaccard_similarity(candidate, reference):
 # for making a frequency table easily
 from collections import Counter
 
-def similarouge1rity(system, reference):
+def rouge1_similarity(system, reference):
     """Returns the ROUGE-1 score between two token lists
 
     Args:
@@ -36,41 +36,38 @@ def similarouge1rity(system, reference):
     Returns:
         float: overlap between the two token lists
     """    
-    
-    ### START CODE HERE (REPLACE INSTANCES OF `None` WITH YOUR CODE) ###
-    
+       
     # make a frequency table of the system tokens (hint: use the Counter class)
-    sys_counter = None
+    sys_counter = Counter(system)
     
     # make a frequency table of the reference tokens (hint: use the Counter class)
-    ref_counter = None
+    ref_counter = Counter(reference)
     
     # initialize overlap to 0
-    overlap = None
+    overlap = 0
     
     # run a for loop over the sys_counter object (can be treated as a dictionary)
     for token in sys_counter:
         
         # lookup the value of the token in the sys_counter dictionary (hint: use the get() method)
-        token_count_sys = None
+        token_count_sys = sys_counter.get(token,0)
         
         # lookup the value of the token in the ref_counter dictionary (hint: use the get() method)
-        token_count_ref = None
+        token_count_ref = ref_counter.get(token,0)
         
         # update the overlap by getting the smaller number between the two token counts above
-        overlap += None
+        overlap += min(token_count_sys, token_count_ref)
     
     # get the precision (i.e. number of overlapping tokens / number of system tokens)
-    precision = None
+    precision = overlap / sum(sys_counter.values())
     
     # get the recall (i.e. number of overlapping tokens / number of reference tokens)
-    recall = None
+    recall = overlap / sum(ref_counter.values())
     
     if precision + recall != 0:
         # compute the f1-score
-        rouge1_score = None
+        rouge1_score = 2 * ((precision * recall)/(precision + recall))
     else:
         rouge1_score = 0 
-    ### END CODE HERE ###
     
     return rouge1_score
