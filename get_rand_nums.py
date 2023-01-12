3227702cfd9bd698435d042fd4ffeafd0848ddf0
Elisei Stakovskii <104836394+eistakovskii@users.noreply.github.com>
2023-01-12 11:00:12 +0300

    Create get_rand_nums.py

---
+++ get_rand_nums.py
@@ -0,0 +1,16 @@
+from random import randint
+
+def get_n_randints(quant: int, t_list: list) -> list:
+    '''
+    Use this function to generate N (quant) number of integers from the range of 0 to X (t_list - 1) numbers
+    
+    Input:
+        quant: number of integers to generate
+        t_list: list to define range of integers to draw from
+
+    Output:
+        _ : a list of with integers
+    '''
+
+    len_thr = len(t_list) - 1
+    return [randint(0, len_thr) for _ in range(quant)]
