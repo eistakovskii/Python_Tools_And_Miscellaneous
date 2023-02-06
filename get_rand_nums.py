a6829aec541c7debd76b73193d1352f259f18bc3
Elisei Stakovskii <104836394+eistakovskii@users.noreply.github.com>
2023-02-06 14:21:02 +0300

    Update get_rand_nums.py

--- get_rand_nums.py
+++ get_rand_nums.py
@@ -1,6 +1,7 @@
 from random import randint
+from random import seed
 
-def get_n_randints(quant: int, t_list: list) -> list[int]:
+def get_n_randints(quant: int, t_list: list, rand_seed = 42) -> list[int]:
     '''
     Use this function to generate N (quant) number of integers from the range of 0 to X (t_list - 1) numbers
     
@@ -11,6 +12,6 @@ def get_n_randints(quant: int, t_list: list) -> list[int]:
     Output:
         _ : a list of with integers
     '''
-
+    seed(rand_seed)
     len_thr = len(t_list) - 1
     return [randint(0, len_thr) for _ in range(quant)]
